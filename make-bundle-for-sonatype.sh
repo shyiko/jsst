81373b2cd23ddaa092c631631ad95e03c798a994
shyiko <stanley.shyiko@gmail.com>
2010-11-07 21:01:09 +0200

    Scripts for making Nexus bundles added

---
+++ make-bundle-for-sonatype.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+GPG_PASSPHRASE=
+TARGET_DIRECTORY=build/oss-release
+VERSION=1.0.3
+
+function copyToOssRelease {
+    for f in $1/*.*; do
+        echo "Copying $f to $TARGET_DIRECTORY";
+        cp $f $TARGET_DIRECTORY
+    done
+}
+
+echo "Preparing files..."
+rm $TARGET_DIRECTORY/*.jar 
+rm $TARGET_DIRECTORY/*.asc
+copyToOssRelease build/dist/lib
+copyToOssRelease build/dist/src
+copyToOssRelease build/dist/docs/javadocs
+
+echo "Running PGP..."
+for f in $TARGET_DIRECTORY/*.*; do
+    echo "Processing $f";
+    echo $GPG_PASSPHRASE|gpg -ab --passphrase-fd 0 --yes --no-tty $f
+done
+
+echo "Making bundle..."
+cd $TARGET_DIRECTORY;
+for artifact in "jsst" "jsst-antlib" "jsst-core" "jsst-junit" "jsst-testng" "jsst-war"
+do
+    jar -cvf ../$artifact-$VERSION-oss-bundle.jar $artifact-$VERSION*    
+done
---
+++ transformPOMs.groovy
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the GNU Lesser General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author stanley.shyiko@gmail.com
+ * @version 2010-11-06
+ */
+private String getVersion() {
+    String buildNumber = (new File("pom.xml").getText() =~ /<build.number>.*?<\/build.number>/)[0]
+    "1.0.${buildNumber[14..-16]}"
+}
+
+private String replaceVersionInformation(String source) {
+    source.replaceAll(/\$\{build.number\}/, {return "3"})
+}
+
+private String prepareForSonatypeOSS(String source) {
+    def root = new XmlParser().parseText(source)
+    def relativePaths = root.get("parent")?.relativePath
+    if (relativePaths) {
+        Node relativePath = relativePaths[0]
+        removeNodeIfExist relativePath.parent(), relativePath.name().getLocalPart()
+    }
+    removeNodeIfExist root, "properties"
+    removeNodeIfExist root, "build"
+    removeNodeIfExist root, "profiles"
+    toString(root)
+}
+
+private void removeNodeIfExist(Node parent, String child) {
+    def children = parent.get(child)
+    if (children)
+        parent.remove((Node) children[0])
+}
+
+private String toString(Node root) {
+    def writer = new StringWriter()
+    def printer = new XmlNodePrinter(new PrintWriter(writer));
+    printer.setPreserveWhitespace true
+    printer.print root
+    writer.toString()
+}
+
+private String moveModulesFromDefaultProfile(String source) {
+    def root = new XmlParser().parseText(source)
+    def defaultProfile = root.profiles.profile.find {it.id.get(0).value().get(0) == "default"}
+    root.append(defaultProfile.modules.get(0))
+    toString(root)
+}
+
+def version = getVersion()
+def root = new File("build/oss-release")
+root.mkdirs()
+root.eachFile {
+    it.delete()
+}
+
+def transform = {String source ->
+    prepareForSonatypeOSS(replaceVersionInformation(source))
+}
+def save = {File file, String content ->
+    print "Saving ${file.getAbsolutePath()}"
+    file.write content
+    println ". Done"
+}
+[
+        "jsst-antlib", "jsst-core", "jsst-junit", "jsst-testng", "jsst-war"
+].each {artifact ->
+    def sourcePom = new File(artifact, "pom.xml")
+    def targetPom = new File(root, "${artifact}-${version}.pom")
+    save targetPom, transform(sourcePom.getText()) 
+}
+save new File(root, "jsst-${version}.pom"), transform(moveModulesFromDefaultProfile(new File("pom.xml").getText()))
